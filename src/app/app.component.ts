bf663ef397f39ef1527bac9c1e2421512abc286e
NilsQuirmbach <=>
2016-11-28 10:33:48 +0100

    bump

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+
+  constructor() {
+    require.ensure([], require => {
+      let r = './app.component';
+      let ctx = require(r);
+
+      console.log(ctx);
+    });
+  }
 }
